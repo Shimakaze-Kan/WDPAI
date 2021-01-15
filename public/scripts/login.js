4bedae1e5eee5dcb2d0746f477171f55c01d2c0b
Shimakaze-Kan <wiktormd@outlook.com>
2021-01-15 01:44:52 +0100

    Minor estetic changes

---
+++ public/scripts/login.js
@@ -0,0 +1,4 @@
+if($('.alert-messages').length)
+{
+    $('.alert-messages').slideDown( 300 ).delay( 5000 ).slideUp( 400 );
+}
--- public/views/login.php
+++ public/views/login.php
@@ -4,26 +4,30 @@
     <link rel="stylesheet" type="text/css" href="public/css/style-mobile.css">
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"
             charset="utf-8"></script>
+    <script type="text/javascript" src="public/scripts/login.js" defer></script>
     <title>LOGIN PAGE</title>
 
 </head>
 <body>
     <div class="container">
+
+        <?php
+        if(isset($messages))
+        {
+            $alertMessage = "";
+            foreach ($messages as $message) {
+                $alertMessage = $alertMessage.$message;
+            }
+
+            echo '<div class="alert-messages"><span>'.$alertMessage.'</span></div>';
+        }
+        ?>
+
         <div class="logo">
             CONNTLY
         </div>
         <div class="login-container">
             <form class="login" action="login" method="POST">
-                <div class="message">
-                    <?php
-                        if(isset($messages))
-                        {
-                            foreach ($messages as $message) {
-                                echo $message;
-                            }
-                        }
-                    ?>
-                </div>
                 <div class="login-caption-container">
                     EMAIL
                     <input name="email" type="text" placeholder="email@email.com">
